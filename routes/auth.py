d7365c98de12d553940638606b2047e1fe67447e
malickl <mahassadiadou@gmail.com>
2026-07-22 11:40:07 +0200

    Ajout de la route login dans routes/auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -2,14 +2,30 @@
 Lot 1 : Identité & agenda
 OAuth Google, connexion, gestion des utilisateurs.
 """
-
-from flask import Blueprint
+from flask import Blueprint, redirect, session
+from google_auth_oauthlib.flow import Flow
 
 auth_bp = Blueprint("auth", __name__)
 
-# @auth_bp.route("/login")
-# def login():
-#     ...
+flow_store = {}
+
+SCOPES = [
+    'https://www.googleapis.com/auth/calendar.readonly',
+    'openid',
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile'
+]
+
+@auth_bp.route("/login")
+def login():
+    flow = Flow.from_client_secrets_file(
+        'credentials.json',
+        scopes=SCOPES,
+        redirect_uri='http://127.0.0.1:5001/callback'
+    )
+    auth_url, state = flow.authorization_url(prompt='select_account')
+    flow_store[state] = flow
+    return redirect(auth_url)
 
 # @auth_bp.route("/callback")
 # def callback():

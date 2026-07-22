c75bede6c026ad5a1d10709d3eba3ef7aa1b9d4d
Gherson Gloria Safouess <safouessghersongloria@gmail.com.com^c>
2026-07-22 14:19:40 +0200

    ajout de la route post login consent

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -4,13 +4,10 @@ Tableau de bord, filtres, suppression de compte.
 """
 
 from flask import Blueprint, session, redirect, url_for, render_template
-from utils.database import check_consent
+from utils.database import check_consent, get_user_meetings, get_user_recordings
 
 dashboard_bp = Blueprint("dashboard", __name__)
 
-
-
-dashboard_bp = Blueprint("dashboard", __name__)
 
 @dashboard_bp.route("/dashboard")
 def dashboard():
@@ -18,7 +15,9 @@ def dashboard():
         return redirect(url_for('auth.login'))
 
     if not check_consent(session['user_id']):
-        return render_template("consentement.html")
+        return render_template("consent.html")
 
-    #TODO: afficher le vrai contenu du dashboard (dépend de KAN-27/28)
-    return "Dashboard normal ici"
+    reunions = get_user_meetings(session['user_id'])
+    dictaphones = get_user_recordings(session['user_id'])
+
+    return render_template("dashboard.html", reunions=reunions, dictaphones=dictaphones)

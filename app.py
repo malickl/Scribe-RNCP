752de7a256be583f2b6116a8c9dafc63aaa4d95c
malickl <mahassadiadou@gmail.com>
2026-07-23 10:27:55 +0200

    Fix: Enregistrement des blueprints dans app.py

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
+import os
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
 from flask import Flask
-# from routes.auth import auth_bp
-# from routes.captation import captation_bp
-# from routes.pipeline import pipeline_bp
-# from routes.dashboard import dashboard_bp
+from routes.auth import auth_bp
+from routes.captation import captation_bp
+from routes.dashboard import dashboard_bp
 
 app = Flask(__name__)
 app.secret_key = "a_remplacer"
 
-# app.register_blueprint(auth_bp)
-# app.register_blueprint(captation_bp)
-# app.register_blueprint(pipeline_bp)
-# app.register_blueprint(dashboard_bp)
+app.register_blueprint(auth_bp)
+app.register_blueprint(captation_bp)
+app.register_blueprint(dashboard_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5001, use_reloader=False)
